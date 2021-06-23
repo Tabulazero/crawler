8db5c06f26c8f33ab38e4481b7ea093bd66ee21c
Tabulazero <Tabulazero@gmail.com>
2021-06-23 20:44:58 +0200

    23.06.2021

--- module/config.js
+++ module/config.js
@@ -1,17 +1,17 @@
 export const crawler = {};
 
-crawler.AttackWithSelection ={
-    Strength: "crawler.AbilityName.Strength",
-    Agility: "crawler.AbilityName.Agility",
-    Intelligence: "crawler.AbilityName.Intelligence"
+crawler.AttackWith ={
+    'Strength': "crawler.AbilityName.Strength",
+    'Agility': "crawler.AbilityName.Agility",
+    'Intelligence': "crawler.AbilityName.Intelligence"
 }
 
-crawler.AttackPatternSelection ={
-    Plus:"+",
-    Multiply:"*"
+crawler.AttackPattern ={
+    'Plus': "+",
+    'Multiply': "*"
 }
 
-crawler.WeaponHandSelection ={
-    Hand1: "crawler.WeaponHand.Hand1",
-    Hand2: "crawler.WeaponHand.Hand2"
+crawler.Hand ={
+    'Hand1': "crawler.WeaponHand.Hand1",
+    'Hand2': "crawler.WeaponHand.Hand2"
 }
